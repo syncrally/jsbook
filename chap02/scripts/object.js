176eb9265281702eeb443fcec4ef01653775a4d4
syncrally <syncrally@gmail.com>
2024-03-10 13:29:13 +0900

    Add object.html and object.js

---
+++ chap02/scripts/object.js
@@ -0,0 +1,8 @@
+let book = {
+    title: 'JavaScript本格入門',
+    price: 2980,
+    publisher: '技術評論社',
+};
+
+console.log(book.title);    //結果：JavaScript本格入門
+console.log(book['title']); //結果：JavaScript本格入門
