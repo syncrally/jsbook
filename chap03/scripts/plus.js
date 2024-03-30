8f9cf8267a6e431b145845f798c144f2c4290c1c
syncrally <syncrally@gmail.com>
2024-03-30 19:35:20 +0900

    Add plus.html and plus.js

---
+++ chap03/scripts/plus.js
@@ -0,0 +1,5 @@
+console.log('Java' + 'Script'); //結果：JavaScript
+console.log('10' + '1');    //結果：101
+console.log(5 + '6');   //結果：56
+let today =new Date();
+console.log(1234 + today);  //結果：1234Sat Mar 30 2024 19:28:56 GMT+0900(日本標準時)
