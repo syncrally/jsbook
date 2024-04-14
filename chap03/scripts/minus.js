dc07061a4f7f7fbc0ef1155fe7a77a1d23fae240
syncrally <syncrally@gmail.com>
2024-04-14 13:55:45 +0900

    Add minus.html and minus.js

---
+++ chap03/scripts/minus.js
@@ -0,0 +1,5 @@
+console.log('Java' - 'Script'); //結果：NaN
+console.log('10' - '1');    //結果：9
+console.log(5 - '6');   //結果：-1
+let today =new Date();
+console.log(1234 - today);  //結果：-1713070366179
