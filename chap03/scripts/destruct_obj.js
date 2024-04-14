a012a3ae575f0d09ea7b6fabbab97d64a8ad2ce6
syncrally <syncrally@gmail.com>
2024-04-14 18:08:53 +0900

    Add destruct_obj.html and destruct_obj.js

---
+++ chap03/scripts/destruct_obj.js
@@ -0,0 +1,10 @@
+let book = {
+    title: 'Javaポケットリファレンス',
+    publisher: '技術評論社',
+    price: 2680,
+};
+
+let {price, title, memo = 'x'} = book;
+console.log(price); //結果：2680
+console.log(title); //結果：Javaポケットリファレンス
+console.log(memo);  //結果：x
