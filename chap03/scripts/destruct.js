034039c3cb59df59262ae60693290b239b274848
syncrally <syncrally@gmail.com>
2024-04-14 17:04:53 +0900

    Add destruct.html and destruct.js

---
+++ chap03/scripts/destruct.js
@@ -0,0 +1,10 @@
+let data = [56, 40, 26, 82, 19, 17, 73, 99];
+let [x0, x1, x2, x3, x4, x5, x6, x7] = data;
+console.log(x0);    //結果：56
+console.log(x1);    //結果：40
+console.log(x2);    //結果：26
+console.log(x3);    //結果：82
+console.log(x4);    //結果：19
+console.log(x5);    //結果：17
+console.log(x6);    //結果：73
+console.log(x7);    //結果：99
