5892fba352a008211309aa3be9838e5aa08b5668
syncrally <syncrally@gmail.com>
2024-04-20 14:58:20 +0900

    Add switch_fall.html and switch_fall.js

---
+++ chap04/scripts/switch_fall.js
@@ -0,0 +1,12 @@
+let rank = 'B';
+
+switch (rank) {
+    case 'A':
+    case 'B':
+    case 'C':
+        console.log('合格！');
+        break;
+    default:
+        console.log('不合格！...');
+        break;
+}   //結果：合格！
