009fe9e264ac8ca9beda178d39d50beb458adb3b
syncrally <syncrally@gmail.com>
2024-04-20 11:34:41 +0900

    Add typeof.html and typeof.js

---
+++ chap03/scripts/typeof.js
@@ -0,0 +1,17 @@
+let num = 1;
+console.log(typeof num);    //結果：number
+
+let str = 'こんにちは';
+console.log(typeof str);    //結果：string
+
+let flag = true;
+console.log(typeof flag);   //結果：boolean
+
+let ary = ['JavaScript', 'PHP', 'Python'];
+console.log(typeof ary);    //結果：object
+
+let obj = {x: 1, y: 2};
+console.log(typeof obj);    //結果：object
+
+let dat = new Date();
+console.log(typeof dat);    //結果：object
