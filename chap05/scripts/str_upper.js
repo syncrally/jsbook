49dc5a1b0cac77976525a126a9821b0b048a2c56
syncrally <syncrally@gmail.com>
2024-08-16 14:07:18 +0900

    Add str_upper.html and str_upper.js

---
+++ chap05/scripts/str_upper.js
@@ -0,0 +1,6 @@
+let str1 = 'Wings';
+let str2 = 'Ｗｉｎｇｓ';
+
+console.log(str1.toLowerCase());    //結果：wings
+console.log(str1.toUpperCase());    //結果：WINGS
+console.log(str2.toLowerCase());    //結果：ｗｉｎｇｓ
