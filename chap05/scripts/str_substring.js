8c2ac12ad7830b38b2e2fd03eef54817e4838943
syncrally <syncrally@gmail.com>
2024-08-17 13:00:13 +0900

    Fix str_substring.js

--- chap05/scripts/str_substring.js
+++ chap05/scripts/str_substring.js
@@ -1,7 +1,7 @@
 let str = 'WINGSプロジェクト';
 
-console.log(str.charAt(5));
-console.log(str.substring(5));
-console.log(str.substring(5,8));
-console.log(str.slice(5));
-console.log(str.slice(5,8));
+console.log(str.charAt(5)); //結果：プ
+console.log(str.substring(5));  //結果：プロジェクト
+console.log(str.substring(5,8));  //結果：プロジ
+console.log(str.slice(5));  //結果：プロジェクト
+console.log(str.slice(5,8));  //結果：プロジ
