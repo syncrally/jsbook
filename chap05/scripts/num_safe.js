4a9f9ee7194658d2f6e39ab58aa73cbb9a5f91f3
syncrally <syncrally@gmail.com>
2024-10-21 14:55:54 +0900

    Add num_safe.html and num_safe.js

---
+++ chap05/scripts/num_safe.js
@@ -0,0 +1,3 @@
+console.log(Number.MAX_SAFE_INTEGER);       //結果：9007199254740991
+console.log(Number.MAX_SAFE_INTEGER + 1);   //結果：9007199254740992
+console.log(Number.MAX_SAFE_INTEGER + 2);   //結果：9007199254740992(不正)
