3a914435e45a7c32200a115c1a08350f3d8e8eaa
syncrally <syncrally@gmail.com>
2024-10-25 15:08:01 +0900

    Add big_int.html and big_int.js

---
+++ chap05/scripts/big_int.js
@@ -0,0 +1,9 @@
+console.log(BigInt(Number.MAX_SAFE_INTEGER) + 2n);  //結果：9007199254740993n
+
+console.log(10n / 3n);
+console.log(10 / 3);
+
+console.log(10n === 10);    //結果：false
+console.log(10n == 10);     //結果：true
+
+console.log(10n + 2);
