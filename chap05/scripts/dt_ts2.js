0ed0b94ebcb2bcd4ed0b7c772f0eec6a14388d05
syncrally <syncrally@gmail.com>
2024-11-09 14:32:04 +0900

    Add dt_ts2.html and dt_ts2.js

---
+++ chap05/scripts/dt_ts2.js
@@ -0,0 +1,4 @@
+let d = new Date();
+console.log(d.getTime());                           //結果：1731130028249
+console.log(Date.now());                            //結果：1731130028249
+console.log(Date.UTC(2022, 11, 4, 20, 7, 15));      //結果：1670184435000
