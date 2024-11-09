62cc55520de5c531ae58b17a1bcc4fc71421e053
syncrally <syncrally@gmail.com>
2024-11-09 14:31:42 +0900

    Add dt_ts.html and dt_ts.js

---
+++ chap05/scripts/dt_ts.js
@@ -0,0 +1,4 @@
+let d = new Date(2022, 11, 4, 20, 7, 15, 368);
+let d2 = new Date(d.getTime());
+console.log(d);     //結果：Sun Dec 04 2022 20:07:15 GMT+0900（日本標準時）
+console.log(d2);    //結果：Sun Dec 04 2022 20:07:15 GMT+0900（日本標準時）
