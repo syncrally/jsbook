23c36577a496d7adf41fe9a01a69cdd19d9b3a49
syncrally <syncrally@gmail.com>
2024-11-10 14:09:23 +0900

    Add dt_get_utc.html and dt_get_utc.js

---
+++ chap05/scripts/dt_get_utc.js
@@ -0,0 +1,9 @@
+let dt = new Date(2022, 11, 4, 20, 7, 15, 368);
+console.log(dt.getUTCFullYear());     //結果：2022
+console.log(dt.getUTCMonth());        //結果：11
+console.log(dt.getUTCDate());         //結果：4
+console.log(dt.getUTCDay());          //結果：0
+console.log(dt.getUTCHours());        //結果：11
+console.log(dt.getUTCMinutes());      //結果：7
+console.log(dt.getUTCSeconds());      //結果：15
+console.log(dt.getUTCMilliseconds()); //結果：368
