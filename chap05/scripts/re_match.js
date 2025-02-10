e30014d8cd6d96fc1d4057e117ba6764016a5161
syncrally <syncrally@gmail.com>
2025-02-10 15:42:51 +0900

    Add re_match.html and re_match.js

---
+++ chap05/scripts/re_match.js
@@ -0,0 +1,8 @@
+let re = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/gi;
+let str = `サポートサイトはhttp://www.example.com/です。
+            サンプル紹介サイトHTTPS://www.web-deli.com/もよろしく！`;
+
+let results = str.match(re);
+for (let result of results) {
+    console.log(result);
+}
