745e1ba19cb432112ff1cbf30a53a0a2ed5c1388
syncrally <syncrally@gmail.com>
2025-02-16 07:28:10 +0900

    Fix re_match.js

--- chap05/scripts/re_match.js
+++ chap05/scripts/re_match.js
@@ -1,4 +1,4 @@
-let re = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/gi;
+let re = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/i;
 let str = `サポートサイトはhttp://www.example.com/です。
             サンプル紹介サイトHTTPS://www.web-deli.com/もよろしく！`;
 
