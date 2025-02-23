2b5de79a39ee1106c9b35a41e6c22ea9462e984d
syncrally <syncrally@gmail.com>
2025-02-23 19:39:06 +0900

    Add re_exec.html and re_exec.js

---
+++ chap05/scripts/re_exec.js
@@ -0,0 +1,8 @@
+let result;
+let re = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w-.\/?%&=]*)?/gi;
+let str = `サポートサイトはhttp://www.example.com/です。
+            サンプル紹介サイトHTTPS://www.web-deli.com/もよろしく！`;
+
+while ((result = re.exec(str)) !== null) {
+    console.log(result);
+}
