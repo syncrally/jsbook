f69f7f2e62c59b46cc76e9ad93a1f785a5503a55
syncrally <syncrally@gmail.com>
2025-04-26 17:01:43 +0900

    Add func_data.html and func_data.js

---
+++ chap06/scripts/func_data.js
@@ -0,0 +1,7 @@
+function getTriangleArea(base, height) {
+    return base * height / 2;
+}
+
+console.log(getTriangleArea(10, 4));    //結果：20
+getTriangleArea = 0;
+console.log(getTriangleArea);   //結果：0
