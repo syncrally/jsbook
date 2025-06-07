30cb9420600a1144aefcf554885d550ff02074d1
syncrally <syncrally@gmail.com>
2025-06-07 17:53:26 +0900

    Add args_variable_mix.html and args_variable_mix.js

---
+++ chap06/scripts/args_variable_mix.js
@@ -0,0 +1,10 @@
+function printf(format, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        let pattern = new RegExp(`\\{${i}\\}`,'g');
+        format = format.replaceAll(pattern, args[i]);
+    }
+    console.log(format);
+}
+
+printf('こんにちは、{0}さん。私は{1}です。','掛谷', '山田');
+    //結果：こんにちは、掛谷さん。私は山田です。
