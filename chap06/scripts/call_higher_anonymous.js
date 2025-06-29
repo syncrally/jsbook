6c0819127592e44d02d38bf74862bce0a3ba8468
syncrally <syncrally@gmail.com>
2025-06-29 19:42:56 +0900

    Add call_higher_anonymous.html and call_higher_anonymous.js

---
+++ chap06/scripts/call_higher_anonymous.js
@@ -0,0 +1,12 @@
+function arrayWalk(data, callback) {
+    for (let [key, value] of data.entries()) {
+        callback(value, key);
+    }
+}
+
+let list = [1,2,4,8,16];
+arrayWalk(
+    list,
+    function (value, key) {
+        console.log(`${key}:${value}`);
+});
